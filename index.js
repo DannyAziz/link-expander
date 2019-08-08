bc40b7c076d9b51a4ba9b93ccf05551b7aaf95ef
Danny Aziz <daanyaal997@gmail.com>
2019-08-08 18:38:45 +0100

    Styling and loop through event.path for A instead of event.target

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 let container = document.createElement("div");
+let textContent = document.createElement("p");
+let opensSpan = document.createElement("span");
+textContent.textContent = ' ';
+opensSpan.textContent = 'Opens: ';
+opensSpan.style.color = '#7D7C7A';
+textContent.style.fontFamily = 'Helvetica';
+textContent.style.color = '#E7E7E7';
+textContent.style.fontSize = '15px';
+textContent.style.lineHeight = '30px';
+textContent.style.margin = 'inherit'
+
+textContent.prepend(opensSpan);
+container.appendChild(textContent);
 
 const mapContainerToMousePosition = (e) => {
     let { pageX, pageY } = e;
@@ -7,28 +20,41 @@ const mapContainerToMousePosition = (e) => {
 }
 
 const createContainer = () => {
-    container.id = 'wanker123';
     container.style.position = 'absolute';
-    container.style.background = 'green';
-    container.style.width = '200px';
-    container.style.height = '200px';
+    container.style.background = '#0F0F0F';
+    container.style.minWidth = '105px';
+    container.style.height = '30px';
+    container.style.padding = '0 10px';
     container.style.pointerEvents = 'none';
-
+    container.style.borderRadius = '2px';
+    container.style.display = 'none';
 
     document.getElementsByTagName("body")[0].appendChild(container);
 };
 
 window.addEventListener('mouseover', (e) => {
-    let { target } = e;
-    if (target.tagName == 'A') {
+    let { path } = e;
+    aTagPresent = false;
+
+    for (elem of path) {
+        if (elem.tagName === 'A') {
+            aTagPresent = true;
+            break;
+        }
+    }
+
+    if (aTagPresent) {
         window.addEventListener("mousemove", mapContainerToMousePosition);
         let { href, target } = e;
         let opensInString = 'This Window';
         if (target == '_blank') {
             opensInString = 'New Tab'
         };
+        textContent.childNodes[1].nodeValue = opensInString;
+        container.style.display = 'block';
     } else {
         window.removeEventListener("mousemove", mapContainerToMousePosition);
+        container.style.display = 'none';
     }
 });
 
