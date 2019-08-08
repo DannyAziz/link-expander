f992b32f125e19da1037f260fbba2047b7b73730
Danny Aziz <daanyaal997@gmail.com>
2019-08-08 17:57:05 +0100

    Container only follows if hovering over link

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 let container = document.createElement("div");
 
-window.addEventListener('mouseover', (e) => {
-    let { target } = e;
-    if (target.tagName == 'A') {
-        let { href, target } = e;
-        let opensInString = 'This Window';
-        if (target == '_blank') {
-            opensInString = 'New Tab'
-        };
-    }
-});
-
-window.addEventListener("mousemove", (e) => {
+const mapContainerToMousePosition = (e) => {
     let { pageX, pageY } = e;
     container.style.top = `${pageY + 20}px`;
     container.style.left = `${pageX + 20}px`;
-});
+}
 
 const createContainer = () => {
     container.id = 'wanker123';
@@ -27,6 +16,20 @@ const createContainer = () => {
 
 
     document.getElementsByTagName("body")[0].appendChild(container);
-}
+};
+
+window.addEventListener('mouseover', (e) => {
+    let { target } = e;
+    if (target.tagName == 'A') {
+        window.addEventListener("mousemove", mapContainerToMousePosition);
+        let { href, target } = e;
+        let opensInString = 'This Window';
+        if (target == '_blank') {
+            opensInString = 'New Tab'
+        };
+    } else {
+        window.removeEventListener("mousemove", mapContainerToMousePosition);
+    }
+});
 
 createContainer();
